7b01177451c7a43987e611e582d98bde257f491c
Sarah Iraurgui <iraurguisj@gmail.com>
2020-11-12 14:32:09 -0500

    mdbootstrap and login compnents added

---
+++ client/src/components/Box.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Subscription from "./Subscription";
+
+function Box() {
+  return (
+    <div>
+        <Subscription />
+    </div>
+  );
+}
+
+export default Box;
