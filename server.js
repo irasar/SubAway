87478046efbe87802e1f54790e4ec6d6e4ea1930
Sarah Iraurgui <iraurguisj@gmail.com>
2020-11-13 12:09:35 -0500

    connection made in server file

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Add routes, both API and view
+app.use(routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/test",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  }
+);
+
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
