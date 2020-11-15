8211856c6e4c0eafbd8984268d62a9d53eb6eee3
Sarah Iraurgui <iraurguisj@gmail.com>
2020-11-15 18:52:52 -0500

    mdbootstrap nav added

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -3,8 +3,8 @@ import React from "react";
 function Jumbotron() {
     return (
         <div className="jumbotron">
-            <h1 className="display-4">Hello, world!</h1>
-            <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <h1 className="display-4">Welcome to SubAway.</h1>
+            <p className="lead">Where we manage your subscriptions so you dont have to.</p>
             <hr className="my-4" />
             <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
             <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
