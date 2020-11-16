defc9ddf6c8a8932a0fe7fabf5cd53864409dc36
Sarah Iraurgui <iraurguisj@gmail.com>
2020-11-16 13:54:34 -0500

    subscription table added

--- client/src/components/Box.js
+++ client/src/components/Box.js
@@ -1,12 +1,92 @@
-import React from "react";
-import Subscription from "./Subscription";
+import React from 'react';
+import { MDBCard, MDBCardBody, MDBCardHeader, MDBInput, MDBBtn, MDBTable, MDBTableBody, MDBTableHead  } from 'mdbreact';
 
-function Box() {
-  return (
-    <div>
-        <Subscription />
-    </div>
+const TablePage = (props) => {
+  const data_panel  = {
+    columns: [
+      {
+        'label': 'First Name',
+        'field': 'first',
+        'sort': 'asc'
+      },
+      {
+        'label': 'Last Name',
+        'field': 'last',
+        'sort': 'asc'
+      },
+      {
+        'label': 'Username',
+        'field': 'username',
+        'sort': 'asc'
+      },
+      {
+        'label': 'Username',
+        'field': 'username2',
+        'sort': 'asc'
+      }
+    ],
+    rows: [
+      {
+        'first': 'Mark',
+        'last': 'Otto',
+        'username': '@mdo',
+        'username2': 'Mark'
+      },
+      {
+        'first': 'Jacob',
+        'last': 'Thornton',
+        'username': '@fat',
+        'username2': 'Jacob',
+     
+      },
+      {
+        'first': 'Larry',
+        'last': 'the Bird',
+        'username': '@twitter',
+        'username2': 'Larry',
+      },
+      {
+        'first': 'Paul',
+        'last': 'Topolski',
+        'username': '@P_Topolski',
+        'username2': 'Paul',
+     
+      }
+    ]
+  };
+
+  return(
+    <MDBCard narrow>
+      <MDBCardHeader className="view view-cascade gradient-card-header blue-gradient d-flex justify-content-between align-items-center py-2 mx-4 mb-3">
+        <div>
+          <MDBBtn outline rounded size="sm" color="white" className="px-2">
+            <i className="fa fa-th-large mt-0"></i>
+          </MDBBtn>
+          <MDBBtn outline rounded size="sm" color="white" className="px-2">
+            <i className="fa fa-columns mt-0"></i>
+          </MDBBtn>
+        </div>
+        <a href="#" className="white-text mx-3">Table name</a>
+        <div>
+          <MDBBtn outline rounded size="sm" color="white" className="px-2">
+            <i className="fas fa-pencil-alt mt-0"></i>
+          </MDBBtn>
+          <MDBBtn outline rounded size="sm" color="white" className="px-2">
+            <i className="fas fa-times mt-0"></i>
+          </MDBBtn>
+          <MDBBtn outline rounded size="sm" color="white" className="px-2">
+            <i className="fa fa-info-circle mt-0"></i>
+          </MDBBtn>
+        </div>
+      </MDBCardHeader>
+      <MDBCardBody cascade>
+        <MDBTable btn fixed>
+          <MDBTableHead columns={data_panel.columns} />
+          <MDBTableBody rows={data_panel.rows} />
+        </MDBTable>
+      </MDBCardBody>
+    </MDBCard>
   );
-}
+};
 
-export default Box;
+export default TablePage;
