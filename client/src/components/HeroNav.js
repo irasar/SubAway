2a3675dfd4e674df92d0baeef49f22ee3de8b1fb
Sarah Iraurgui <iraurguisj@gmail.com>
2020-11-15 22:03:27 -0500

    navbar compondents made

--- client/src/components/HeroNav.js
+++ client/src/components/HeroNav.js
@@ -50,10 +50,8 @@ class FullPageIntroWithFixedTransparentNavbarx extends React.Component {
 
           <MDBView src="https://mdbootstrap.com/img/Photos/Others/img%20(40).jpg">
             <MDBMask overlay="purple-light" className="flex-center flex-column text-white text-center">
-              <h2>This Navbar is fixed</h2>
-              <h5>It will always stay visible on the top, even when you scroll down</h5>
-              <p>Navbar's background will switch from transparent to solid color while scrolling down</p><br />
-              <p>Full page intro with background image will be always displayed in full screen mode, regardless of device </p>
+              <h1>Welcome to SubAway</h1>
+              <h3>Where we manage your subscriptions so you dont have to.</h3>
             </MDBMask>
           </MDBView>
         </header>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,14 +2,10 @@ import React from "react";
 import FullPageIntroWithFixedTransparentNavbarx  from "../components/HeroNav"
 import LoginButton from "../components/LoginButton";
 import LogoutButton from "../components/LogoutButton";
-import FullPageIntroWithFixedTransparentNavbar  from "../components/Navbar"
-// import ButtonPage from "../components/Button";
 
 function Home() {
     return (
         <div>
-            {/* <ButtonPage /> */}
-            {/* <Navbar /> */}
             <FullPageIntroWithFixedTransparentNavbarx />
             <LoginButton />
             <LogoutButton />
