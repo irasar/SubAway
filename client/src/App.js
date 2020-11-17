541da208c19465cd1f07d994b64af19756ce090b
tiroxxx <christianmarquez41@gmail.com>
2020-11-16 23:12:19 -0500

    added protected route and moved navbar to its own component, also added loading animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home"
-import Member from "./pages/Member"
+import Home from "./pages/Home";
+import Member from "./pages/Member";
+import Loading from "./components/loading";
+import PrivateRoute from "./components/private-route";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
 
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/member">
-            <Member />
-          </Route>
-        </Switch>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <PrivateRoute exact path="/member" component={Member} />
+      </Switch>
     </Router>
-
-
-
   );
 }
 
