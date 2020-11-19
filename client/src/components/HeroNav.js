41d89e96b3248b44b3181514eeecbc877b83fab8
Sarah Iraurgui <iraurguisj@gmail.com>
2020-11-19 12:11:50 -0500

    added content to transition into moving background

--- client/src/components/HeroNav.js
+++ client/src/components/HeroNav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBMask,
+import { MDBAnimation, MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBMask,
 MDBRow, MDBCol, MDBIcon, MDBBtn, MDBView, MDBContainer } from "mdbreact";
 import Navbar from "./Navbar";
  
@@ -33,22 +33,38 @@ return (
       <source src="https://mdbootstrap.com/img/video/animation.mp4" type="video/mp4" />
     </video>
     <MDBMask className="d-flex justify-content-center align-items-center gradient">
-      <MDBContainer className="px-md-3 px-sm-0">
-        <MDBRow>
-          <MDBCol md="12" className="mb-4 white-text text-center">
-            <h3 className="display-3 font-weight-bold mb-0 pt-md-5">
-            Welcome to SubAway{" "}
-            </h3>
-            <hr className="hr-light my-4 w-75" />
-            <h4 className="subtext-header mt-2 mb-4">
-            Where we manage your subscriptions so you dont have to.
-            </h4>
-            <MDBBtn outline rounded color="white">
-              <MDBIcon icon="home" /> Get Started
-            </MDBBtn>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
+      {<MDBContainer>
+              <MDBRow>
+                <MDBCol
+                  md="6"
+                  className="white-text text-center text-md-left mt-xl-5 mb-5"
+                >
+                  <MDBAnimation type="fadeInLeft" delay=".3s">
+                    <h1 className="h1-responsive font-weight-bold mt-sm-5">
+                    Welcome to SubAway
+                    </h1>
+                    <hr className="hr-light" />
+                    <h6 className="mb-4">
+                    Where we manage your subscriptions so you dont have to.
+                    </h6>
+                 
+                    <MDBBtn outline color="white">
+                     Get Started
+                    </MDBBtn>
+                  </MDBAnimation>
+                </MDBCol>
+
+                <MDBCol md="6" xl="5" className="mt-xl-5">
+                  <MDBAnimation type="fadeInRight" delay=".3s">
+                    <img
+                      src="https://mdbootstrap.com/img/Mockups/Transparent/Small/admin-new.png"
+                      alt=""
+                      className="img-fluid"
+                    />
+                  </MDBAnimation>
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>}
     </MDBMask>
   </MDBView>
 
